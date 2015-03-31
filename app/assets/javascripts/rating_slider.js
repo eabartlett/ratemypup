38fa65d5415620573d6c7335fe159c96823b8432
annagoodman <aegoodman@berkeley.edu>
2015-03-30 22:02:05 -0700

    woo the numbers are dynamically updating

--- app/assets/javascripts/rating_slider.js
+++ app/assets/javascripts/rating_slider.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
           $("#slider-breeder").slider({
               animate: true,
-              value:1,
-              min: 1,
+              value: 0,
+              min: 0,
               max: 5,
               step: 1,
               slide: function(event, ui) {
@@ -13,8 +13,8 @@ $(document).ready(function() {
 
           $("#slider-health").slider({
               animate: true,
-              value:1,
-              min: 1,
+              value: 0,
+              min: 0,
               max: 5,
               step: 1,
               slide: function(event, ui) {
@@ -23,8 +23,8 @@ $(document).ready(function() {
           });
           $("#slider-train").slider({
               animate: true,
-              value:1,
-              min: 1,
+              value: 0,
+              min: 0,
               max: 5,
               step: 1,
               slide: function(event, ui) {
@@ -33,8 +33,8 @@ $(document).ready(function() {
           });
           $("#slider-social").slider({
               animate: true,
-              value:1,
-              min: 1,
+              value: 0,
+              min: 0,
               max: 5,
               step: 1,
               slide: function(event, ui) {
@@ -43,8 +43,8 @@ $(document).ready(function() {
           });
           $("#slider-energy").slider({
               animate: true,
-              value:1,
-              min: 1,
+              value: 0,
+              min: 0,
               max: 5,
               step: 1,
               slide: function(event, ui) {
@@ -58,13 +58,11 @@ $(document).ready(function() {
           $("#train").val(0);
           $("#social").val(0);
           $("#energy").val(0);
-          $("#amount-label").text(1);
-          $("#duration-label").text(1);
-          $("#breeder-label").text(1);
-          $("#health-label").text(1);
-          $("#train-label").text(1);
-          $("#social-label").text(1);
-          $("#energy-label").text(1);
+          $("#breeder-label").text(0);
+          $("#health-label").text(0);
+          $("#train-label").text(0);
+          $("#social-label").text(0);
+          $("#energy-label").text(0);
           
           update();
       });
@@ -72,8 +70,6 @@ $(document).ready(function() {
       //changed. now with parameter
       function update(slider,val) {
         //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
-        var $amount = slider == 1?val:$("#amount").val();
-        var $duration = slider == 2?val:$("#duration").val();
         var $breeder = slider == 1?val:$("#breeder").val();
         var $health = slider == 2?val:$("#health").val();
         var $train = slider == 3?val:$("#train").val();
@@ -84,15 +80,22 @@ $(document).ready(function() {
         $duration = $( "#slider2" ).slider( "value" );
          */
 
-         // $total = "$" + ($amount * $duration);
-         // $( "#amount" ).val($amount);
-         // $( "#amount-label" ).text($amount);
-         // $( "#duration" ).val($duration);
-         // $( "#duration-label" ).text($duration);
-         // $( "#total" ).val($total);
-         // $( "#total-label" ).text($total);
-         $('#slider-breeder a').html('<label><span class="fa fa-paw"></span>');
-         $('#slider-health a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$health+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
-         $('#slider-train a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$train+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
-         // $('#slider-breeder a').html('<label><span class="fa fa-paw"></span>');
+        $("#breeder-label").text($breeder);
+        $("#health-label").text($health);
+        $("#train-label").text($train);
+        $("#social-label").text($social);
+        $("#energy-label").text($energy);
+
+        // $total = "$" + ($amount * $duration);
+        // $( "#amount" ).val($amount);
+        // $( "#amount-label" ).text($amount);
+        // $( "#duration" ).val($duration);
+        
+        // $( "#total" ).val($total);
+        // $( "#total-label" ).text($total);
+
+        $('#slider-breeder a').html('<label><span class="fa fa-paw"></span>');
+        $('#slider-health a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$health+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+        $('#slider-train a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$train+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+        // $('#slider-breeder a').html('<label><span class="fa fa-paw"></span>');
       }
