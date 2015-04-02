bb6e569ba20326d91ad27c044cd41a7d4f68ac54
annagoodman <aegoodman@berkeley.edu>
2015-04-02 14:22:46 -0700

    things are broken

--- features/step_definitions/pup_steps.rb
+++ features/step_definitions/pup_steps.rb
@@ -128,3 +128,22 @@ def slide(slidr, value)
   page.execute_script "s.slider('option', 'value', #{value})"
   #page.execute_script "s.slider('option','slide').call(s,null,{ handle: $('.ui-slider-handle', s), value: #{value} });"
 end
+
+Given /^a confirmation box saying "(.*)" should pop up$/ do |message|
+  @expected_message = message
+end
+
+Given /^I want to click "(.*)"$/ do |option|
+  retval = (option == "Ok") ? "true" : "false"
+
+  page.evaluate_script("window.confirm = function (msg) {
+    $.cookie('confirm_message', msg)
+    return #{retval}
+  }")
+end
+
+Then /^the confirmation box should have been displayed$/ do
+  page.evaluate_script("$.cookie('confirm_message')").should_not be_nil
+  page.evaluate_script("$.cookie('confirm_message')").should eq(@expected_message)
+  page.evaluate_script("$.cookie('confirm_message', null)")
+end
